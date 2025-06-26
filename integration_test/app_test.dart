24d8303c6ee0c3b1ed0a32d5dc6e2f9c5cc275a9
annd <annd@closerworks.com>
2025-06-26 15:24:45 +0700

    test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -51,4 +51,17 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('4'), findsOneWidget);
   });
+
+  testWidgets("5", (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.text('0'), findsNothing);
+    expect(find.text('5'), findsOneWidget);
+  });
 }
